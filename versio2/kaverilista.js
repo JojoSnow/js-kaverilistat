f763a7b7d1b6571e94fbfb292c9c0f29c88c231a
JojoSnow <milzwow94@gmail.com>
2021-04-29 14:19:21 +0300

    JS toimii

--- versio2/kaverilista.js
+++ versio2/kaverilista.js
@@ -1,11 +1,15 @@
-let nameArray = []
+const nameArray = [];
 let nameForm = document.forms['nameForm'];
+const addButton = document.querySelector('.addButton');
+const deleteButton = document.querySelector('.deleteButton');
+const sortButton = document.querySelector('.sortButton');
 
-nameForm.addEventListener('submit', addName)
-nameForm.addEventListener('submit', deleteName)
+addButton.addEventListener('click', addName);
+deleteButton.addEventListener('click', deleteName);
+sortButton.addEventListener('click', sortList);
 
 function addName(event) {
-    event.preventDefault()
+    event.preventDefault();
     
     let nameFriend = document.querySelector('#main input[type="text"]').value;
     
@@ -13,38 +17,66 @@ function addName(event) {
         alert('Pitää antaa nimi!');
         return;
     }
-    nameArray.unshift(nameFriend);
-    
-    for(let i = 0; i < nameArray.length; i++) {
-        let newLiName = document.createElement('li')
-        let newLiText = document.createTextNode(nameArray[i])
-        newLiName.appendChild(newLiText)
-        newLiName.className = 'friend-list-item';
-        document.querySelector('#friendList').appendChild(newLiName)
-    }
+    nameArray.push(nameFriend);
+    console.log(nameArray);
+
+    const newLiName = document.createElement('li');
+    const newLiText = document.createTextNode(nameArray[nameArray.length - 1]);
+    newLiName.appendChild(newLiText);
+    newLiName.className = 'friend-list-item';
+    document.querySelector('#friendList').appendChild(newLiName);
 }
 
 function deleteName(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     let nameFriend = document.querySelector('#main input[type="text"]').value;
+    let nameIndex = (nameArray.indexOf(nameFriend));
+    let deleteLiParent = document.querySelector('#friendList');
+    let deleteLi = document.querySelectorAll('li');
 
     if(nameFriend.length < 1){
         alert('Pitää antaa nimi!');
         return;
     }
 
-    for(let i = 0; i < nameArray.length; i++){
-        if(nameArray[i] === nameFriend){
-            nameArray.splice(i, 1);
-        }
+    if(nameArray.includes(nameFriend)){
+        nameArray.splice(nameIndex, 1);
     }
 
+    for(let i = 0; i < nameArray.length + 1; i++) {
+        deleteLiParent.removeChild(deleteLi[i]);
+    }
+    
     for(let i = 0; i < nameArray.length; i++) {
-        let newLiName = document.createElement('li')
-        let newLiText = document.createTextNode(nameArray[i])
-        newLiName.appendChild(newLiText)
+        if(nameArray.length === 0) {
+            break;
+        }
+        const newLiName = document.createElement('li');
+        const newLiText = document.createTextNode(nameArray[i]);
+        newLiName.appendChild(newLiText);
         newLiName.className = 'friend-list-item';
-        document.querySelector('#friendList').appendChild(newLiName)
+        document.querySelector('#friendList').appendChild(newLiName);
+    }
+}
+
+function sortList(event) {
+    event.preventDefault();
+    
+    let deleteLiParent = document.querySelector('#friendList');
+    let deleteLi = document.querySelectorAll('li');
+    
+    for(let i = 0; i < nameArray.length; i++) {
+        deleteLiParent.removeChild(deleteLi[i]);
+    }
+
+    nameArray.sort();
+
+    for(let i = 0; i < nameArray.length; i++) {
+        const newLiName = document.createElement('li');
+        const newLiText = document.createTextNode(nameArray[i]);
+        newLiName.appendChild(newLiText);
+        newLiName.className = 'friend-list-item';
+        document.querySelector('#friendList').appendChild(newLiName);
     }
 }
